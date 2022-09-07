053e4bb892e0bae503c9ba70c2f33a2f56a3d02a
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-09-07 01:02:10 -0500

    Se agrega primer diseño de codigo qr

--- src/components/CodigoQr.jsx
+++ src/components/CodigoQr.jsx
@@ -1,16 +1,65 @@
 import React, { useState, useEffect, Fragment } from "react";
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ImageBanner from '../img/in_posible_banner.png'; // Import using relative path
+
 
 export default function CodigoQr(props) {
 
     return (
-        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <Grid item xs={12} sm={12} md={12}>
-                <img class="card-img-top" src={"data:image/jpg;base64,"+props.codigoQr}  />
+        
+        <Grid container justifyContent="center">
+            <Grid item xs={12} sm={12} md={9}>
+                    <Grid container sx={{minHeight:'10vh', maxHeight:'25vh', minWidth:'40vh', maxWidth:'155vh'}} justifyContent="center">
+                        <Grid
+                            item
+                            xs={12}
+                            sm={4}
+                            md={4}
+                            sx={{
+                                backgroundImage: `url(${ImageBanner})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                            }}
+                        />
+                    </Grid>
             </Grid>
             <Grid item xs={12} sm={12} md={12}>
-                {props.name}
+                <Box component="main" sx={{ flex: 1, py: 2, px: 3}}>
+                    <Grid container rowSpacing={2}>
+                        <Grid item xs={12} xm={12} md={12}>
+                            <Paper sx={{maxWidth: 300, maxHeight: 300, margin: 'auto', overflow: 'hidden' }} style={{borderRadius: 26}} >
+                                <Grid container rowSpacing={2}>
+                                    <Grid item>
+                                        <img class="card-img-top" src={"data:image/jpg;base64,"+props.codigoQr} style={{height:"45%", margin:"33px"}}/>
+                                    </Grid>
+                                </Grid> 
+                                
+                            </Paper>
+                        </Grid>
+                        
+                    </Grid>
+                    
+                    
+                </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                >
+                    <Typography style={{color: 'white'}} gutterBottom variant="h3">
+                        {props.name}
+                    </Typography>
+                    
+                </Box>
+                
             </Grid>
         </Grid>
+        
     )
 }
--- src/modules/Registro.jsx
+++ src/modules/Registro.jsx
@@ -4,10 +4,10 @@ import CodigoQr from '../components/CodigoQr'
 
 export default function Registro() {
 
-    const [isCreated, setIsCreated] = useState(false);
-    const [codigoQr, setCodigoQr] = useState('');
-    const [name, setName] = useState('');
-   
+    const [isCreated, setIsCreated] = useState(true);
+    const [codigoQr, setCodigoQr] = useState('iVBORw0KGgoAAAANSUhEUgAAAcIAAAHCAQAAAABUY/ToAAADw0lEQVR4nO2cTY6rOBRGz22QMnSkWkCWYnZQS2q9JdUOYClZQEkwLMno64FtMEn35HVFrxLdb4AIcEQsXdn3z5j4PU1//SYITjrppJNOOumkkz+PtKK+XJ3OAKzGZGY2ADYs9anhW97p5KuQSJKIkqS5k6REPox0AjpBSNS79eHxucbp5OPJZZ9uTjI7d7K/59WA1aTrSfnuPlU96Tid/H6yv70wXVI+0XQRFj/ekgEmlu96p5MvTsZ5NY2LGfFqxnReTZLyPPSgdzr5EmSQNObTTsS5kw3ZFZJsYDWNkB2lb3unky9FTmZmdgZYemzY7sdrDyynbEhmZvZd73TyRcgcl+2CkABKhJZ/xrmrodsmj8ucrCrh+gxAJ41B5aAZShy//SzG5Tbk5K5DXBZmjLD2xHntiR+gyQACGEEAq+nP/VsnfyJZ5qHiSZPXrTLx1HwiUXVBy0ubz0NOtmpMhVCS1dUBqsnqOJPPPE/t5L3yWqZ8WAzlKtkloek8izi/oVI+A2vI5xqnk48jsw1Z/HhLEFJf3R6KZ0T4MhVH6dMEa+/+kJP/Tq5mw3KSdDWzgdXIYdpyUs5Ys/TkQtrw5/+tkz+QDJJ+Xb7MhiDBclLJNoavsoAVj3v1HKOTRzX+ECw9Ynkr5Yw4glhOYnpPNQewvMn+7zudfC2yyTHWPHXJJ9YcYzrG+wnPMTp5VFu/yKaSiuXknPTelzZDves25GSjvUqmcWtXHKFpZixljpCA4PkhJ29VVqattMpNUnGGYmG1KcTXMiePqja0HbZrjfnss08tgrgNObmp2kSqTnRz6Jp6WelQc3/IyTvt0VhbIGtqaKntXowelzl5p9an3jcDQd0MNEPbiLZ1o7kNOVlVbAi2SWabeI5RftOc5jbkZKuSY6yJxqYLZAzbGTQW5vOQkwftNYwEQTAZED/6ZPFqaHpfe6LWnsm6w3dmnmucTj6OPNQ6ZuCuqbr1kXbC5yEnq7aaq4AgjDADS58st5yFr9JAHUcwwmeduZ5rnE4+mty/+yFde4DVSmZx7nIXSD7EuvP+Ocfp5APJWBPTNrBatRwz/do3JYYE0+XL+4ecvNHuD+31sr0836aogT10c3/Iyaq7737EcTWx9HlrmSYDiwKDLtl0Ed6D5uRBdzbEcgbCZ2+lE3+G6fzZa3qfEazuUzt5VLWIIGABTecuWZzXsl9oGvpk8dpj0CcjzN4L6+SNmlpHkwYKpdZR80OlfOb+kJP3OvTkt2dl2yIh1Wv5bHvgucbppJNOOumkk046+V/6BwdVR7Amw1vXAAAAAElFTkSuQmCC');
+    const [name, setName] = useState('Brayan Neyra');
+    
     return isCreated? 
         <CodigoQr codigoQr={codigoQr} name={name}/>
         : <Formulario setIsCreated={setIsCreated} setCodigoQr={setCodigoQr}  setName={setName}/>
